4670f5dc3946fd05b2f70afe6b2e1864de44b9ba
이호식 <leehosick462589@gmail.com>
2023-09-08 06:42:18 +0900

    [FEAT] : ⚙️메세지 보내기 기능 구현

--- app/src/main/java/com/example/clearlink/ContactDetailFragment.kt
+++ app/src/main/java/com/example/clearlink/ContactDetailFragment.kt
@@ -20,6 +20,7 @@ class ContactDetailFragment : AppCompatActivity() {
 
     private lateinit var binding : ActivityContactDetailFragmentBinding
     private val CALL_REQUEST_CODE = 1
+    private val MESSAGE_REQUEST_CODE = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityContactDetailFragmentBinding.inflate(layoutInflater)
@@ -62,6 +63,35 @@ class ContactDetailFragment : AppCompatActivity() {
                 val intent = Intent(Intent.ACTION_CALL)
                 intent.data = Uri.parse("tel:$phoneNumber")
 
+                startActivity(intent)
+            }
+        }
+
+        //메세지 보내기 버튼
+
+        val messageButton = findViewById<Button>(R.id.contact_detail_fragment_btn_message)
+
+        messageButton.setOnClickListener {
+            val phoneNumber = item.phoneNumber
+
+            // SEND_SMS 권한을 확인합니다.
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.SEND_SMS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // 권한을 요청합니다.
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.SEND_SMS),
+                    MESSAGE_REQUEST_CODE
+                )
+            } else {
+                // 권한이 이미 허용된 경우 메시지를 보내기 위한 Intent를 생성합니다.
+                val intent = Intent(Intent.ACTION_SENDTO)
+                intent.data = Uri.parse("smsto:$phoneNumber") // smsto: 프로토콜을 사용하여 SMS 메시지를 보냅니다
+
+                // 메시지를 보내기 위한 액티비티를 시작합니다.
                 startActivity(intent)
             }
         }
