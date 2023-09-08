0380162300c41f263dc2b1ab4857156e73bc8459
jiyeon <cn0714@naver.com>
2023-09-08 12:20:18 +0900

    [DESIGN] : 🎨 Recyclerview item

--- app/src/main/java/com/example/clearlink/adapter/ContactListAdapter.kt
+++ app/src/main/java/com/example/clearlink/adapter/ContactListAdapter.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ItemTouchHelper
@@ -66,7 +67,7 @@ class ContactListAdapter : RecyclerView.Adapter<ContactListAdapter.ViewHolder>()
         private val binding: RvItemBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        val swipeLayout : LinearLayout
+        val swipeLayout : ConstraintLayout
 
         init {
             binding.contactListRecyclerviewFavorites.setOnClickListener {
@@ -103,6 +104,7 @@ class ContactListAdapter : RecyclerView.Adapter<ContactListAdapter.ViewHolder>()
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
 
         //클릭 이벤트 추가
         holder.itemView.setOnClickListener {
